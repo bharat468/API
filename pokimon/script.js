d9731d834903985167bea0d8ece02fe5d3752bc8
bharat <bharatpareek256@gmail.com>
2025-10-08 16:56:29 +0530

    lastfainal

--- pokimon/script.js
+++ pokimon/script.js
@@ -1,5 +1,9 @@
 let url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
 let finalData = []
+let offset = 0
+let input = document.querySelector("#searchbyname")
+let main = document.querySelector("main")
+let button = document.querySelector(".button")
 
 async function fatchUrl(fatchtourl) {
     let reserve = await fetch(fatchtourl)
@@ -22,8 +26,35 @@ async function fatchnew(data) {
     return finalData
 }
 
-async function displayData(){
-
+async function displayData(urldata) {
+    main.innerHTML = ""
+    for (let i = 0; i < urldata.length; i++) {
+        let div = document.createElement("div")
+        div.classList.add("div")
+        let name = urldata[i].name
+        let image = document.createElement("img")
+        image.classList.add("image")
+        image.src = urldata[i].sprites.other.dream_world.front_default;
+        // console.log(image);
+        main.append(div)
+        div.append(image)
+        div.append(name)
+    }
 }
 
+button.addEventListener("click", async () => {
+    offset = offset + 20;
+    let data = await fatchUrl(
+        "https://pokeapi.co/api/v2/pokemon?limit=20&offset=" + offset
+    );
+    console.log(data);
+    displayData(await fatchnew(data));
+})
+
+input.addEventListener("keyup", async (e) => {
+    let lastwork = finalData.filter((last) => {
+        return last.name.includes(e.target.value)
+    })
+    displayData(lastwork)
+})
 
