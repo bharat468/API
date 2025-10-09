bed0e96842f6e7d296b3b713f48d33f42c130ea8
bharat <bharatpareek256@gmail.com>
2025-10-09 14:11:39 +0530

    newupdatefile

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 let url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
-let finalData = []
-let offset = 0
+let newUrl = "https://pokeapi.co/api/v2/type/?limit=21"
+let finalData = [];
+let offset = 0;
+let search = [];
+// console.log(search);
+let select = document.querySelector("#types")
 let input = document.querySelector("#searchbyname")
 let main = document.querySelector("main")
 let button = document.querySelector(".button")
+let options = [];
 
 async function fatchUrl(fatchtourl) {
     let reserve = await fetch(fatchtourl)
     let result = await reserve.json()
+    // console.log(result);
+
     return result
 }
 
 window.addEventListener("load", async () => {
     let data = await fatchUrl(url)
     displayData(await fatchnew(data))
+    newApi()
+
+
 })
 
 async function fatchnew(data) {
@@ -27,6 +37,7 @@ async function fatchnew(data) {
 }
 
 async function displayData(urldata) {
+
     main.innerHTML = ""
     for (let i = 0; i < urldata.length; i++) {
         let div = document.createElement("div")
@@ -35,10 +46,23 @@ async function displayData(urldata) {
         let image = document.createElement("img")
         image.classList.add("image")
         image.src = urldata[i].sprites.other.dream_world.front_default;
+        let type = urldata[i].types[0].type.name
+        let para = document.createElement("p")
+        para.append(type)
+
+        // console.log(type);
+        // let type = document.createElement("p");
+        // type.classList.add("type");
+
+
         // console.log(image);
         main.append(div)
         div.append(image)
         div.append(name)
+
+
+
+
     }
 }
 
@@ -57,4 +81,41 @@ input.addEventListener("keyup", async (e) => {
     })
     displayData(lastwork)
 })
+
+select.addEventListener("change", (e) => {
+    let pokemonname = finalData.filter((last) => {
+        const category = last.types[0].type.name;
+
+        return category.includes(e.target.value)
+    })
+    displayData(pokemonname)
+})
+
+
+
+async function newApi() {
+    let newpokimon = await fetch(newUrl)
+    let namematch = await newpokimon.json()
+    // console.log(namematch);
+
+    for (let i = 0; i < namematch.results.length; i++) {
+        let last = namematch.results[i].name;
+        search.push(last)
+        // console.log(search);
+        // console.log(last);
+
+        let option = document.createElement("option")
+        option.textContent = last;
+        option.value = last;
+        // console.log(option);
+        options.push(option.value)
+        // console.log(options);
+
+        select.append(option)
+    }
+}
+
+
+
+
 
